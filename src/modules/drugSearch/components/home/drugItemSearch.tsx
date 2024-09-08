27e7e9d95f3737d989729d27cf4015e4fb71c62b
Alisson Rego <alissondroid07@gmail.com>
2024-09-07 23:47:00 -0300

    feat: open modal from drug search

--- src/modules/drugSearch/components/home/drugItemSearch.tsx
+++ src/modules/drugSearch/components/home/drugItemSearch.tsx
@@ -1,22 +1,28 @@
 import { PillBottle, ChevronRight } from "lucide-react"
 import { Drug } from "../../interfaces/drug"
+import { useProductDetails } from "../../hooks/useProductDetails"
 
 interface IDrugItemSearch {
     drugSearch: Drug
 }
 
 function DrugItemSearch({ drugSearch }: IDrugItemSearch) {
+    const { isOpenProductDetails, renderModal } = useProductDetails()
     return (
-        <div className="group w-full h-fit flex items-center gap-3 pr-3 rounded-xl hover:bg-indigo-100/30 overflow-hidden cursor-pointer">
-            <div className="w-[42px] h-[42px] bg-orange-600 flex items-center justify-center rounded-xl">
-                <PillBottle className="text-white size-5" />
+        <>
+            <div onClick={() => isOpenProductDetails.set(true)} className="group w-full h-fit flex items-center gap-3 pr-3 rounded-xl hover:bg-indigo-100/30 overflow-hidden cursor-pointer">
+                <div className="w-[42px] h-[42px] bg-orange-600 flex items-center justify-center rounded-xl">
+                    <PillBottle className="text-white size-5" />
+                </div>
+                <div className="flex-1 flex flex-col items-start gap-0.5">
+                    <h2 className="text-zinc-950 font-semibold line-clamp-1">{drugSearch.brand_name}</h2>
+                    <p className="text-[#ADB5BD] text-xs line-clamp-1">{drugSearch.labeler_name}</p>
+                </div>
+                <ChevronRight className="hidden group-hover:block text-zinc-950 size-6" />
             </div>
-            <div className="flex-1 flex flex-col items-start gap-0.5">
-                <h2 className="text-zinc-950 font-semibold line-clamp-1">{drugSearch.brand_name}</h2>
-                <p className="text-[#ADB5BD] text-xs line-clamp-1">{drugSearch.labeler_name}</p>
-            </div>
-            <ChevronRight className="hidden group-hover:block text-zinc-950 size-6" />
-        </div>
+
+            {isOpenProductDetails.value === true && renderModal(drugSearch)}
+        </>
     )
 }
 
