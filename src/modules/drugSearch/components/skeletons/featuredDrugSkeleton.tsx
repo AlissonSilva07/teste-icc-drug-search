d363d78dd4ccd663e41dd23e11ffb3b95e6be167
Alisson Rego <alissondroid07@gmail.com>
2024-09-08 02:28:19 -0300

    style: change the color of skeletons

--- src/modules/drugSearch/components/skeletons/featuredDrugSkeleton.tsx
+++ src/modules/drugSearch/components/skeletons/featuredDrugSkeleton.tsx
@@ -1,14 +1,14 @@
 function FeaturedDrugSkeleton() {
     return (
         <div className="w-full h-fit flex items-center gap-6 animate-pulse">
-            <div className="w-[257px] h-[180px] bg-[#ADB5BD] rounded-xl"></div>
+            <div className="w-[257px] h-[180px] bg-indigo-100 rounded-xl"></div>
             <div className="flex flex-col items-start gap-2.5">
-                <div className="p-1 bg-[#ADB5BD] text-[#ADB5BD] rounded-lg">
+                <div className="p-1 bg-indigo-100 text-indigo-100 rounded-lg">
                     Latest
                 </div>
-                <h2 className="p-1 bg-[#ADB5BD] text-[#ADB5BD]">information for skeleton large text</h2>
-                <p className="p-w bg-[#ADB5BD] text-[#ADB5BD]">information for skeleton</p>
-                <div className="px-6 py-3 bg-[#ADB5BD] text-[#ADB5BD] rounded-full">skeleton</div>
+                <h2 className="p-1 bg-indigo-100 text-indigo-100 rounded-xl">information for skeleton large text</h2>
+                <p className="p-w bg-indigo-100 text-indigo-100 rounded-xl">information for skeleton</p>
+                <div className="px-6 py-3 bg-indigo-100 text-indigo-100 rounded-full">skeleton</div>
             </div>
         </div>
     )
