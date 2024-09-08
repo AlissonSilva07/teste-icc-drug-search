b8a7dad2c9896b43b9a9f7edbdbd30761d3f21b5
Alisson Rego <alissondroid07@gmail.com>
2024-09-07 23:39:40 -0300

    feat: open modal from drug item

--- src/modules/drugSearch/components/home/drugItem.tsx
+++ src/modules/drugSearch/components/home/drugItem.tsx
@@ -1,24 +1,30 @@
 import { PillBottle, ChevronRight } from "lucide-react"
 import { IngredientsGroup } from "./ingredientsGroup"
 import { Drug } from "../../interfaces/drug"
+import { useProductDetails } from "../../hooks/useProductDetails"
 
 interface IDrugItem {
     drug: Drug
 }
 
 function DrugItem({ drug }: IDrugItem) {
+    const { isOpenProductDetails, renderModal } = useProductDetails()
     return (
-        <div className="group w-full h-fit flex items-center gap-6 pr-3 rounded-xl hover:bg-indigo-100/30 overflow-hidden cursor-pointer">
-            <div className="w-[148px] h-[98px] bg-orange-600 flex items-center justify-center rounded-xl">
-                <PillBottle className="text-white size-8" />
+        <>
+            <div onClick={() => isOpenProductDetails.set(true)} className="group w-full h-fit flex items-center gap-6 pr-3 rounded-xl hover:bg-indigo-100/30 overflow-hidden cursor-pointer">
+                <div className="w-[148px] h-[98px] bg-orange-600 flex items-center justify-center rounded-xl">
+                    <PillBottle className="text-white size-8" />
+                </div>
+                <div className="flex-1 flex flex-col items-start gap-2.5">
+                    <h2 className="text-zinc-950 font-semibold text-2xl line-clamp-1">{drug.brand_name}</h2>
+                    <p className="text-[#ADB5BD] line-clamp-1">{drug.labeler_name}</p>
+                    {drug.active_ingredients ? <IngredientsGroup ingredients={drug.active_ingredients} /> : <p className="text-indigo-800 uppercase text-[10px] font-semibold">No ingredients available</p>}
+                </div>
+                <ChevronRight className="hidden group-hover:block text-zinc-950 size-6" />
             </div>
-            <div className="flex-1 flex flex-col items-start gap-2.5">
-                <h2 className="text-zinc-950 font-semibold text-2xl line-clamp-1">{drug.brand_name}</h2>
-                <p className="text-[#ADB5BD] line-clamp-1">{drug.labeler_name}</p>
-                {drug.active_ingredients ? <IngredientsGroup ingredients={drug.active_ingredients} /> : <p className="text-indigo-800 uppercase text-[10px] font-semibold">No ingredients available</p>}
-            </div>
-            <ChevronRight className="hidden group-hover:block text-zinc-950 size-6" />
-        </div>
+
+            {isOpenProductDetails.value === true && renderModal(drug)}
+        </>
     )
 }
 
--- src/shared/components/modalProductDetails.tsx
+++ src/shared/components/modalProductDetails.tsx
@@ -12,7 +12,7 @@ interface IModalSearch {
 function ModalProductDetails({ drug, onClose }: IModalSearch) {
     return (
         <main className="fixed inset-0 bg-zinc-950/30 flex justify-center z-50">
-            <div className="w-2/5 h-fit mt-20 p-6 flex flex-col gap-6 bg-white rounded-xl">
+            <div className="w-2/5 h-fit mt-20 p-6 flex flex-col items-start gap-6 bg-white rounded-xl">
                 <div className="w-full flex items-center justify-between">
                     <h2 className="text-2xl text-zinc-950 font-semibold">Product Details</h2>
                     <button onClick={onClose} className="p-1 rounded-full hover:bg-indigo-100">
@@ -20,7 +20,7 @@ function ModalProductDetails({ drug, onClose }: IModalSearch) {
                     </button>
                 </div>
 
-                <div>
+                <div className="w-full">
                     <div className="w-full h-fit flex items-center gap-6">
                         <div className="w-[257px] h-[128px] bg-orange-600 flex items-center justify-center rounded-xl">
                             <PillBottle className="text-white size-12" />
@@ -42,20 +42,20 @@ function ModalProductDetails({ drug, onClose }: IModalSearch) {
                     </div>
                 </div>
                 <Separator />
-                <div className="flex flex-col gap-3">
+                <div className="w-full flex flex-col gap-3">
                     <h3 className="text-xl text-zinc-950 font-semibold">Drug Info</h3>
-                    <div className="p-3 flex flex-col gap-3 bg-[#DEE2E6]/30 rounded-xl">
+                    <div className="w-full p-3 flex flex-col gap-3 bg-[#DEE2E6]/30 rounded-xl">
                         <p className="text-zinc-950">Product ID: <span className="font-semibold text-indigo-800">{drug.product_id}</span></p>
                         <p className="text-zinc-950">Marketing Type: <span className="font-semibold text-indigo-800">{drug.marketing_category}</span></p>
                         <p className="text-zinc-950">Dosage Form: <span className="font-semibold text-indigo-800">{drug.dosage_form}</span></p>
                         <p className="text-zinc-950">Expiration Date: <span className="font-semibold text-indigo-800">{dayjs(drug.listing_expiration_date).format('YYYY-MM-DD')}</span></p>
                     </div>
                 </div>
-                <div className="flex flex-col gap-3">
+                <div className="w-full flex flex-col gap-3">
                     <h3 className="text-xl text-zinc-950 font-semibold">Active ingredients</h3>
                     <div className="w-full flex items-center gap-6 flex-wrap">
                         {drug.active_ingredients.length > 0 ?
-                            drug.active_ingredients.map(aI => <ActiveIngredientCard key={aI.name} ingredient={aI} />)
+                            drug.active_ingredients.map(aI => <ActiveIngredientCard key={aI.name} ingredient={aI} />).slice(0, 6)
                             : <p>No ingredients available.</p>}
                     </div>
                 </div>
